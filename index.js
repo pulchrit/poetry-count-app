94fafcdf84f7b2651d6f9e13783c63905bc70069
Melissa Lafranchise <pulchrit@gmail.com>
2019-04-07 11:17:01 -0700

    Correctly fetching data from API for multiple poets and tracking if compare is checked on/off

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 'use strict';
 
-// Storing poet data in this global variable such that it
-// can be processes easily for the charts and poem viewer.
-// Array of one array if single poet, or array or many
-// arrays if multiple poets.
-const poetResponseData = [];
 
 // Opens/closes 'Search poets' and 'Predefined searches' on mobile/smaller screens.
 // May not be necessary for larger screens.
@@ -20,6 +15,11 @@ function toggleCollapsibleMenus() {
         $(".js-poet-search").toggleClass("searches-collapsible searches-active");
     });
 }
+
+// Initializing this global variable for the array that contains 
+// the aggregate data for all poets. 
+// Not sure of the better way to do this and avoid a global variable??
+//const allDataForAllPoets = [];
 
 // Won't need this function if I stay with PoetryDb, but 
 // leaving it in for now just in case. 
@@ -52,38 +52,43 @@ function constructPoetryDBUrl(poet) {
     }; */
 }
 
-function getPoetData(poet) {
+function processAllData(allDataForAllPoets, compare) {
+    console.log("data from process function: ");
+    console.log(allDataForAllPoets);
+    console.log("compare:" + compare);
+  
+}
+
+function getPoetData(poets, compare) {
+
+    const allDataForAllPoets = [];
+
+    // PoetryDB API allows only one poet to be queried at a time,
+    // so, if multiple poets have been entered, we need to loop 
+    // through them and call the API for each. Resulting data for 
+    // each is pushed into the allDataForAllPoets array.
+    poets.forEach(poet => {
     
-    fetch(constructPoetryDBUrl(poet)) // adjust if this doesn't work, trying to do what jeremy said
-    .then(response => {
-        if (!response.ok) {
-            throw new Error(response.statusText);
-        }
-        return response.json(); 
-    })   
-    //.then(responseJSON => processResults(responseJSON, states))  
-    .catch(error => {
-        $(".js-results").html(
-            `<p>Hmmm...Something isn't right. Here's the error message:</p>
-            <p>${error}</p>`);
-        $(".js-results").removeClass("hidden");
-    })    
-}
+        fetch(constructPoetryDBUrl(poet))
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.statusText);
+            }
+            return response.json(); 
+        })
+        .then(responseJSON => {
+            allDataForAllPoets.push(responseJSON);
+            return allDataForAllPoets;
+        }) 
+        .catch(error => {
+            $(".js-results").html(
+                `<p>Hmmm...Something isn't right. Here's the error message:</p>
+                <p>${error}</p>`);
+            $(".js-results").removeClass("hidden");
+        })    
+    });
 
-function getDataForAllPoets(poets) {
-
-    // .map poets array, call getPoetData for each poet, 
-    // push API response data to poetResponseData array. 
-    poetResponseData.push(poets.map(poet => {
-        getPoetData(poet);
-    }));
-
-    return poetResponseData;
-}
-
-function processResults(poetResponseData, boolean) {
-    console.log("compare?" + boolean);
-    console.log("poetResponseData:" + poetResponseData);
+    processAllData(allDataForAllPoets, compare);
 }
 
 function onPoetsEntered() {
@@ -95,25 +100,22 @@ function onPoetsEntered() {
         // so that we can make individual calls to the
         // PoetyDB API. 
         const poets = $("#poets").val().split(",");
-        console.log(poets);
 
-        /* // .map poets array, call getPoemData for each poet, 
-        // push API response data to poetResponseData array. 
-        poetResponseData.push(poets.map(poet => {
-            getPoemData(poet);
-        })); */
-
-
-        // If compare is checked, then pass a true flag to processResults.
+        // If compare is checked, then save compare variable as true.
         // Attribution: https://stackoverflow.com/questions/2834350/get-checkbox-value-in-jquery
+        // Initializing compare as false which is the value that will be passed
+        // if the comparison checkbox is NOT checked.
+        let compare = false;
         if ($("#compare-poets").is(":checked")) {
-       // if ($("#compare-poets").val() === "yes") {
-            processResults(getDataForAllPoets(poets), true);
+            compare = true;
         }
-        processResults(getDataForAllPoets(poets), false);
 
-        // Empty input box
+        // Get all of the data for individual and multiple poets.
+        getPoetData(poets, compare);
+
+        // Empty input box, uncheck checkbox.
         $("#poets").val("");
+        $("#compare-poets").prop("checked", false);
     });
 }
 
